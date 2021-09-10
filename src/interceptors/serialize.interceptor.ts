05170ed57bdfb5dd0ba38d6add12c445ca13957c
HunainBinSajid <hunainbinsajid@gmail.com>
2021-09-10 20:23:08 +0500

    interceptor, serialization and dtos in user module

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,29 @@
+import {
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToClass } from 'class-transformer';
+
+interface ClassConstructor {
+  new (...args: any[]): Record<string, any>;
+}
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: ClassConstructor) {}
+
+  intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
+    return handler.handle().pipe(
+      map((data: any) => {
+        return plainToClass(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
